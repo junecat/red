d99b1216d7c2e8ed6af4cc253a68d4c143146ee0
junecat <junecat@gmail.com>
2025-05-26 18:29:16 +0300

    Got it to working condition

--- red.cpp
+++ red.cpp
@@ -14,6 +14,10 @@ int main(){
         cout << ": " << line << endl;
         lines.push_back(line);
     }
+    size_t maxx = 0;
+    for( auto l : lines )
+        maxx = l.length()>maxx ? l.length() : maxx;
+
     cout << "finished!" << endl;
     cout << lines[0] << endl;
     size_t container_poz = lines[0].find("CONTAINER");
@@ -25,9 +29,12 @@ int main(){
     size_t ports_poz = lines[0].find("PORTS");
     size_t names_poz = lines[0].find("NAMES");
 
-    cout << container_poz << ", "  << id_poz << ", " << image_poz << ", " << command_poz << ", " << created_poz << ", " << status_poz << ", " << ports_poz << ", " << names_poz << endl;
+    cout << container_poz << ", "  << id_poz << ", " << image_poz << ", " << command_poz << ", " << created_poz << ", " << status_poz << ", " << ports_poz << ", " << names_poz << ", " << maxx << endl;
     
-
-
+    for(auto l : lines) {
+        auto x = l.substr(0, ports_poz+140) + "..." + l.substr(names_poz);
+        cout << x << endl;
+    }
+    cout << "finished-2!" << endl;
 
 }
