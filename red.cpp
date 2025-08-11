4086578e60ee1f017684fb0b3566a2e92b7cc082
junecat <junecat@gmail.com>
2025-08-11 14:45:28 +0300

    add determine screen width

--- red.cpp
+++ red.cpp
@@ -2,15 +2,26 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
 using namespace std;
 
 // CONTAINER ID   IMAGE                        COMMAND                  CREATED         STATUS                 PORTS  NAMES
 
 int main(){
+    // get screen width
+    int screen_width = 180;
+    struct winsize w;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) != -1) {
+        if (w.ws_col>=180) 
+            screen_width = w.ws_col;
+    }
+
+    // collect lines
     string line; 
     vector<string> lines;
-    while ( getline(cin, line)  ){            // && !line.empty()
+    while ( getline(cin, line)  ) {            // && !line.empty()
         lines.push_back(line);
     }
     size_t maxx = 0;
@@ -29,7 +40,7 @@ int main(){
     // cout << container_poz << ", "  << id_poz << ", " << image_poz << ", " << command_poz << ", " << created_poz << ", " << status_poz << ", " << ports_poz << ", " << names_poz << ", " << maxx << endl;
     
     for(auto l : lines) {
-        auto x = l.substr(0, ports_poz+140) + "..." + l.substr(names_poz);
+        auto x = l.substr(0, ports_poz+(screen_width-143)) + "..." + l.substr(names_poz);
         cout << x << endl;
     }
 }
