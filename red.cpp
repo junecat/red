7fd5350c4009a4f2968653f93a0bf9f75ce5e074
junecat <junecat@gmail.com>
2025-08-11 11:52:05 +0300

    working with fixed width

--- red.cpp
+++ red.cpp
@@ -11,15 +11,12 @@ int main(){
     string line; 
     vector<string> lines;
     while ( getline(cin, line)  ){            // && !line.empty()
-        cout << ": " << line << endl;
         lines.push_back(line);
     }
     size_t maxx = 0;
     for( auto l : lines )
         maxx = l.length()>maxx ? l.length() : maxx;
 
-    cout << "finished!" << endl;
-    cout << lines[0] << endl;
     size_t container_poz = lines[0].find("CONTAINER");
     size_t id_poz = lines[0].find("ID");
     size_t image_poz = lines[0].find("IMAGE");
@@ -29,12 +26,10 @@ int main(){
     size_t ports_poz = lines[0].find("PORTS");
     size_t names_poz = lines[0].find("NAMES");
 
-    cout << container_poz << ", "  << id_poz << ", " << image_poz << ", " << command_poz << ", " << created_poz << ", " << status_poz << ", " << ports_poz << ", " << names_poz << ", " << maxx << endl;
+    // cout << container_poz << ", "  << id_poz << ", " << image_poz << ", " << command_poz << ", " << created_poz << ", " << status_poz << ", " << ports_poz << ", " << names_poz << ", " << maxx << endl;
     
     for(auto l : lines) {
         auto x = l.substr(0, ports_poz+140) + "..." + l.substr(names_poz);
         cout << x << endl;
     }
-    cout << "finished-2!" << endl;
-
 }
